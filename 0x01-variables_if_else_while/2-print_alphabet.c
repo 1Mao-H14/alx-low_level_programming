f6c7a118cc8e6306ea1ea5d2ebea0e499bee54f0
marwan <marwanhamdaoui2020@gmail.com>
2023-08-13 15:12:58 -0700

    my third commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  * discription:'print alphabets lowercase'
+  * Return: always 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	putchar(n);
+	n++;
+	putchar('\n');
+	return (0);
