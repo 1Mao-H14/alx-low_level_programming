a777bd3052658159007b1fe67d3beec105e7d252
marwan <marwanhamdaoui2020@gmail.com>
2023-08-13 16:53:49 -0700

    lets go bebe

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  * discription:'get the last digit of a number'
+  * Return: always 0
+  */
+int main(void)
+{
+	int n = 97;
+	int N = 65;
+
+	while (n <= 122)
+	{
+		putchar (n);
+		i++;
+	}
+		putchar ("\n")
+
+	while (N <= 90)
+	{
+		putchar(n);
+		i++;
+	}
+	return (0);
+}
