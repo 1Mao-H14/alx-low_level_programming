339dfd877d8deef9546c5ddf364ab6980f2a75a4
marwan <marwanhamdaoui2020@gmail.com>
2023-08-14 08:58:56 -0700

    my 8 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  * discription:'get the last digit of a number'
+  * Return: always 0
+  */
+int main(void)
+{
+	int n;
+	int c;
+
+	for (n = 48 ; n <= 57; n++)
+	{
+	putchar (n);
+	}
+	for (c = 97; c <= 102 ; c++)
+	{
+	putchar (c);
+	}
+	return (0);
+}
