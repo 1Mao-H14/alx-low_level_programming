cbbe363c51935c0f9eb6d7a53f9486eec06529c3
marwan <marwanhamdaoui2020@gmail.com>
2023-08-14 07:14:54 -0700

    My fourth commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  * discription:'get the last digit of a number'
+  * Return: always 0
+  */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+
+	{
+
+		printf("%i", n);
+		n++;
+	}
+		printf("\n");
+	return (0);
+}
