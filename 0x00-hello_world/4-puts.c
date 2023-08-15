a0d249cc8ca872b7b3958d91f224ea205281a46e
marwan <marwanhamdaoui2020@gmail.com>
2023-08-14 17:34:01 -0700

    my 5 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * description : a c programe thats prints with put function
+ * return : Always 0 (succes)
+*/
+
+int main(void)
+{
+puts("\n"Programming is like building a multilingual puzzle");
+return (0);
+}
