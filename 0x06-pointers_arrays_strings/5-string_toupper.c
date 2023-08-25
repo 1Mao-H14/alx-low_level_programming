0919d11b6567af88e6c1386dccfabfbd50c3ab9f
marwan <marwanhamdaoui2020@gmail.com>
2023-08-24 18:36:56 -0700

    my first commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * String_toupper - Change all loweRcase to upPercase
+ *
+ * @x: pointer
+ *
+ * Return: x
+ */
+char *string_toupper(char *x)
+{
+	int a;
+
+	a = 0;
+	while (x[a] != '\0')
+	{
+		if (x[a] >= 'a' && x[a] <= 'z')
+			x[a] = x[a] - 32;
+		a++;
+	}
+	return (x);
+}
