787c431e839bf7f95631da28b67157caed5d246d
Your Name <you@example.com>
2023-08-26 15:46:03 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
- * main-Entry point
- * description : a c programe thats prints with put function
- * return : Always 0 (succes)
+ *main -its anEntry point
+ *description : a c programe thats prints with put function
+ *return : Always 0 (succes)
 */
 
 int main(void)
 {
-puts("\n"Programming is like building a multilingual puzzle");
-return (0);
+puts("\n" Programming is like building a multilingual puzzle");
+
+return(0);
+
 }
