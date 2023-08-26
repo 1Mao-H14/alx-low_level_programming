eca2ff9e8cf2b9860600edc0084fdfce12ad6c10
Your Name <you@example.com>
2023-08-26 16:55:31 -0700

    my fourthe comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- *main -its anEntry point
- *description : a c programe thats prints with put function
- *return : Always 0 (succes)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\n" Programming is like building a multilingual puzzle");
-
-return(0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
