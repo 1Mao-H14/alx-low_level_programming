1a71f97cc00943726268d6883ec18784b0e9bc46
Your Name <you@example.com>
2023-08-28 17:02:01 -0700

    my dirst commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+ *
+ * @a: Starting Address of memory to be filled
+ * @d: The desired value
+ * @s: number of bytes to  CHanged
+ *
+ * Return: changed array with new value for s bytes
+ */
+char *_memset(char *a, char d, unsigned int s)
+{
+	int h = 0;
+
+	for (; s > 0; i++)
+	{
+		a[s] = d;
+		s--;
+	}
+	return (a);
+}
