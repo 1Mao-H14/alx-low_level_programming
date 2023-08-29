db17ff4c539d48b057a6c2290cf64ca52d4a39f0
Your Name <you@example.com>
2023-08-28 17:57:06 -0700

    my eight commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ *
+ * @a: array
+ *
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b;
+	int x;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (x = 0; x < 8; x++)
+			_putchar(a[b][x]);	
+		_putchar('\n');
+	}
+}
