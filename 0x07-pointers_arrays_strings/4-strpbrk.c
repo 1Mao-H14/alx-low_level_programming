c637878534fef89883ad3efeccb150725a3466bb
Your Name <you@example.com>
2023-08-28 17:39:21 -0700

    my fifthe commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ *
+ * @s: Input
+ *
+ * @accept: Input
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int c;
+
+		while (*s)
+		{
+			for (c = 0; accept[c]; c++)
+			{
+			if (*s == accept[c])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
