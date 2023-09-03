3709a1b79f093645f5366caec262eae4b3798cfa
Your Name <you@example.com>
2023-09-03 16:46:17 -0700

    creating a libray with functions

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+ *
+ * @s: Starting Address of memory to be filled
+ * @b: The desired value
+ * @n: number of bytes to  CHanged
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int h = 0;
+
+	for (; n > 0; h++)
+	{
+		s[h] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ *
+ * @dest: Input value
+ *
+ * @src: Input value
+ *
+ * @m: Input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int m)
+{
+	int h;
+
+	h = 0;
+	while (h < m && src[h] != '\0')
+	{
+		dest[h] = src[h];
+		h++;
+	}
+	while (h < m)
+	{
+		dest[h] = '\0';
+		h++;
+	}
+
+	return (dest);
+}
