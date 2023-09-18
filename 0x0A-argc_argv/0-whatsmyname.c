b78f0fe0c3ea731d246af902321ab4dc6fca2fb1
marwan <marwanhamdaoui2020@gmail.com>
2023-09-18 14:51:22 -0700

    my first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - function thats prints the name of the program plus a new line
+ *@argc : integer input
+ *@argv :array argument char input
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("the name of the programme : %s\n", argv[0]);
+	return (0);
+}
