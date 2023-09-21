c120f3c4bc62b88b0a48f0d9a08a1cdb570a375d
marwan <marwanhamdaoui2020@gmail.com>
2023-09-21 11:04:46 -0700

    my commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,61 +2,61 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer function
+ * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int c, a, w, lnt, h, d;
+	int v, x, w, high, l, flo;
 
-	c = 0;
-	a = 0;
+	v = 0;
+	x = 0;
 	w = 0;
-	lnt = 0;
-	h = 0;
-	d = 0;
+	high = 0;
+	l = 0;
+	flo = 0;
 
-	while (s[lnt] != '\0')
-		lnt++;
+	while (s[high] != '\0')
+		high++;
 
-	while (c < lnt && h == 0)
+	while (v < high && l == 0)
 	{
-		if (s[c] == '-')
-			++a;
+		if (s[v] == '-')
+			++x;
 
-		if (s[c] >= '0' && s[c] <= '9')
+		if (s[v] >= '0' && s[v] <= '9')
 		{
-			d = s[c] - '0';
-			if (d % 2)
-				d = -d;
-			w = w * 10 + d;
-			h = 1;
-			if (s[c + 1] < '0' || s[c + 1] > '9')
+			flo = s[v] - '0';
+			if (x % 2)
+				flo = -flo;
+			w = w * 10 + flo;
+
+			l = 1;
+			if (s[v + 1] < '0' || s[v + 1] > '9')
 				break;
-			h = 0;
+			l = 0;
 		}
-		c++;
+		v++;
 	}
 
-	if (h == 0)
+	if (l == 0)
 		return (0);
 
 	return (w);
 }
 
 /**
- * main - multiplies two  numbers a function
+ * main - multiplies two numbers
  * @argc: number of arguments
- *
  * @argv: array of arguments
  *
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int value, first_num, second_num;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -64,11 +64,12 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
+	first_num = _atoi(argv[1]);
+	second_num = _atoi(argv[2]);
+	value = first_num * second_num;
 
-	printf("%d\n", result);
+	printf("%d\n", value);
 
 	return (0);
 }
+
