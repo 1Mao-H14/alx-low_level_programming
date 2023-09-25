b183f1b3e3e3f0726a5b1d917081d84fef10f3fa
marwan <marwanhamdaoui2020@gmail.com>
2023-09-25 14:55:04 -0700

    my 10 commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * insert_nodeint_at_index - a function inserts a new node at a given position
  * @head: THATS PoInt FOR The First Node
- * @idx: is the index of the list where the new node should be added. Index starts at 0
+ * @idx: is the index of the list where the new node should be added
  * @n: data THAT BEEN Inserted in the new node
  *
  * Return: POINTER to the NEW node, or NULL
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (V = 0; TTT && V < idx; V++)
 	{
-		if (V == idx - 1)
+		if (V == 1 - idx)
 		{
 			OLD->next = TTT->next;
 			TTT->next = OLD;
