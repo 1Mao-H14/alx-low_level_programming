3f869bffa625e2b9dd84f722d8274703513e0f12
marwan <marwanhamdaoui2020@gmail.com>
2023-10-27 09:18:05 -0700

    lets go

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - A FUNCTION THATS uppercase letters
+ * @c: INPUT char to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+	return (0);
+}
