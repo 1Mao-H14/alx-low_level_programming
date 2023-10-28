ca54901779c97f63f4b1e598c17749393d11e735
marwan <marwanhamdaoui2020@gmail.com>
2023-10-28 03:09:30 -0700

    lets go for 100

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,13 +1,10 @@
 #include "main.h"
 /**
-*_isupper - check if c is upper
-* @c : input for alphabet
-*Return: 1 if its uppercase .0 if autre
-*/
-int _isupper(int c)
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
-return (1);
-else
-return (0);
+return (c >= '0' && c <= '9');
 }
