db4a76d90996f7792db2c7f329f53c46cb84a042
marwan <marwanhamdaoui2020@gmail.com>
2023-11-05 13:18:38 -0800

    lets go

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+/**
+ * string_nconcat - Concatenate two strings with a limit.
+ * @s1: The first string.
+ * @s2: The second string (up to n characters).
+ * @n: The maximum number of characters from s2 to concatenate.
+ *
+ * Return: A pointer to the concatenated string or NULL on failure.
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *vivo;
-	unsigned int xxx,hig = n;
+	unsigned int xxx, hig = n;
 
 	if (s1 == NULL)
 		s1 = "";
