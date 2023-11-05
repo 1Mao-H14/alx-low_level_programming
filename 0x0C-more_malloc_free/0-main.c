e392cb124838eb23681726b494d9ba857a487b29
marwan <marwanhamdaoui2020@gmail.com>
2023-11-05 10:09:29 -0800

    lets go

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -2,24 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-/**
- * malloc_checked - Allocates memory and checks for success
- * @size: The size (in bytes) of the memory block to allocate
- *
- * Return: A pointer to the allocated memory block on success
- *         Exits with an error message on failure
- */
 
-void *malloc_checked(size_t size)
-{
-void *ptr = malloc(size);
-if (ptr == NULL)
-{
-fprintf(stderr, "Memory allocation failed\n");
-exit(EXIT_FAILURE);
-}
-return (ptr);
-}
 /**
  * main - check the code
  *
@@ -27,22 +10,23 @@ return (ptr);
  */
 int main(void)
 {
-char *c;
-int *i;
-float *f;
-double *d;
+    char *c;
+    int *i;
+    float *f;
+    double *d;
 
-c = malloc_checked(sizeof(char) * 1024);
-printf("%p\n", (void *)c);
-i = malloc_checked(sizeof(int) * 402);
-printf("%p\n", (void *)i);
-f = malloc_checked(sizeof(float) * 100000000);
-printf("%p\n", (void *)f);
-d = malloc_checked(INT_MAX);
-printf("%p\n", (void *)d);
-free(c);
-free(i);
-free(f);
-free(d);
-return (0);
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+    i = malloc_checked(sizeof(int) * 402);
+    printf("%p\n", (void *)i);
+    f = malloc_checked(sizeof(float) * 100000000);
+    printf("%p\n", (void *)f);
+    d = malloc_checked(INT_MAX);
+    printf("%p\n", (void *)d);
+    free(c);
+    free(i);
+    free(f);
+    free(d);
+    return (0);
 }
+
--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: creat array of size size and assign char c
- * Return: pointer to array, NULL if fail
+ * malloc_checked - Allocates memory using malloc
+ * @b: The amount of memory to allocate
+ *
+ * Return: A pointer to the allocated memory Exits with status 98 on failure
  */
-char *create_array(unsigned int size, char c)
+void *malloc_checked(unsigned int b)
 {
-char *str;
-unsigned int i;
+void *block;
 
-str = malloc(sizeof(char) * size);
-if (size == 0 || str == NULL)
-return (NULL);
-for (i = 0; i < size; i++)
-str[i] = c;
-return (str);
+block = malloc(b);
+if (block == NULL)
+	exit(98);
+return (block);
 }
