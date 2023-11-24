1a31d362989bf31b922a2241bbc798bb63e01a65
marwan <marwanhamdaoui2020@gmail.com>
2023-11-24 14:45:25 -0800

    first commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits -  a function thats counts the number of bits to change
+ * to get from one number to another
+ * @n: FIRst num
+ * @m: SECond num
+ *
+ * Return: number bits change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int index, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (index = 63; index >= 0; index--)
+	{
+		current = exclusive >> index;
+		if (current & 1)
+			countbit++;
+	}
+
+	return (countbit);
+}
