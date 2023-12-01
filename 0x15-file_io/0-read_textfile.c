044ecc92da7f383f556c51eb673fb51a0f8b3b5e
marwan <marwanhamdaoui2020@gmail.com>
2023-12-01 13:54:22 -0800

    lets go

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,27 +10,27 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_descriptor;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
+	file_descriptor = open(filename, O_RDONLY);
 
-    if (file_descriptor == -1)
-        return (0);
+	if (file_descriptor == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * (letters));
-    if (!buffer)
-        return (0);
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
+		return (0);
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-    close(file_descriptor);
-    free(buffer);
+	close(file_descriptor);
+	free(buffer);
 
-    return (bytes_written);
+	return (bytes_written);
 }
